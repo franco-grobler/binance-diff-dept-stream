84654af40fcf6f2aa57406472e5050d648497908
Franco Grobler <franco@grobler.fyi>
2026-02-03 20:34:56 +0200

    feat: initial setup
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/franco-grobler/binance-diff-stream
+
+go 1.25.5
---
+++ pkg/binanace-stream/doc.go
@@ -0,0 +1,3 @@
+// Package binanacestream provides interfaces and structs for
+// reading and parsing data from the Binance Websocket stream.
+package binanacestream

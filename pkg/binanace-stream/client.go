fe63c6507669e0db87f1bc5a45fa64e63b895710
Franco Grobler <franco@grobler.fyi>
2026-02-04 20:54:36 +0200

    feat(binance-stream): basic client
    Signed-off-by: Franco Grobler <franco@grobler.fyi>

---
+++ pkg/binanace-stream/client.go
@@ -0,0 +1,16 @@
+package binanacestream
+
+import "net/http"
+
+type StreamClient interface {
+	DepthStream(symbol string, updateSpeed UpdateSpeed) (OrderBook, error)
+}
+
+// HTTPClient Define the interface for the HTTP client's behavior
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+type Client struct {
+	HTTPClient HTTPClient
+}
